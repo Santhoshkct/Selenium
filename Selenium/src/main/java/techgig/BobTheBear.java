bb9336f852627179c1542a22f607f3ae98b48ac4
Santhosh Kumar <38687031+Santhoshkct@users.noreply.github.com>
2018-05-09 00:14:28 +0530

    New change on prg Bob

--- Selenium/src/main/java/techgig/BobTheBear.java
+++ Selenium/src/main/java/techgig/BobTheBear.java
@@ -12,26 +12,34 @@ import java.util.TreeSet;
 
 public class BobTheBear {
 
-	int FishCountonTime = 0;
-	List<Integer> li = new ArrayList<Integer>();
+	private static boolean compare(List<List<Integer>> firstlist, List<Integer> store) {
+		for (List<Integer> list : firstlist) 
+			if(list.equals(store))
+				return false;
 
-	BobTheBear(int count,int salmon)
-	{
-		this.FishCountonTime = count;
-		li.add(salmon);
+		return true;
 	}
+
+	private static boolean comparesort(List<Integer> comp, List<Integer> first) {
+		for(int i=0;i<first.size();i++)
+			for(int j=0;j<comp.size();j++)
+				if(first.get(i) == comp.get(j))
+					return false;
+
+		return true;
+	}	
 
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
 		int salmonCount = scan.nextInt();
-		int[] len = new int[salmonCount];
+		long[] len = new long[salmonCount];
 		for(int i=0;i<salmonCount;i++) {
-			len[i] = scan.nextInt();
+			len[i] = scan.nextLong();
 		}
-		int[] time = new int[salmonCount];
+		long[] time = new long[salmonCount];
 		for(int i=0;i<salmonCount;i++) {
-			time[i] = scan.nextInt();
+			time[i] = scan.nextLong();
 		}
 		scan.close();
 
@@ -39,54 +47,71 @@ public class BobTheBear {
 			System.out.println(salmonCount);
 		else 
 		{
-			Map<Integer,BobTheBear> fishcatch = new TreeMap<Integer,BobTheBear>();
+			System.out.println("Start");
+			Map<Integer,long[]> fish = new TreeMap<Integer,long[]>();
 
-			int[] arr = new int[2]; 
+
+			long max = 0;
 			for(int i=0;i<salmonCount;i++)
 			{
+				long[] arr = new long[2];
 				arr[0] = time[i];
 				arr[1] = time[i] + len[i];
-				for(int j=arr[0];j<=arr[1];j++)
-				{
-					if(fishcatch.containsKey(j))
-					{
-						BobTheBear exist = fishcatch.get(j);
-						exist.FishCountonTime = exist.FishCountonTime + 1;
-						exist.li.add(i);
-					}
-					else
-						fishcatch.put(j,new BobTheBear(1,i));		
+				fish.put(i, arr);
+				if(arr[1] > max)
+					max = arr[1];
+			}
+
+			for (Entry<Integer, long[]> c : fish.entrySet())
+			{	
+				System.out.print(c.getKey() + "-->");
+				for (long l : c.getValue()) {
+					System.out.print(l + " ");
 				}
+				System.out.println();
+			}
+
+			List<List<Integer>> firstlist = new ArrayList<List<Integer>>();
+
+			for(long j=0;j<=max;j++)
+			{
+				List<Integer> store = new ArrayList<Integer>();
+				int count = 0;
+				for (Entry<Integer, long[]> check : fish.entrySet())
+				{
+					long[] value = check.getValue();
+					if(j >= value[0] && j<= value[1])
+					{
+						store.add(check.getKey());
+						count++;
+					}
+				}
+				if(count != 0)
+					if(firstlist.isEmpty())
+						firstlist.add(store);
+					else if(compare(firstlist,store))
+						firstlist.add(store);
+			}
+
+			for (List<Integer> li : firstlist) {
+				System.out.println(li);
 			}
 
 			Set<Integer> possiblities = new TreeSet<Integer>();
-			List<Integer> firstlist = new ArrayList<Integer>();
 
-			int i = 1;
-			while(!fishcatch.isEmpty())
+			int maxi = 0;
+			for(int i=0;i<firstlist.size();i++)
 			{
-				for (Entry<Integer, BobTheBear> c : fishcatch.entrySet()) 
-					if(c.getKey() == i)
-						firstlist = c.getValue().li;
-					else if(compare(c.getValue().li,firstlist))
-						possiblities.add(c.getValue().li.size() + firstlist.size());
-
-				fishcatch.remove(i);
-				i++;
+				if(firstlist.get(i).size() > maxi)
+					maxi = firstlist.get(i).size();
+				for(int j=i+1;j<firstlist.size();j++)
+					if(comparesort(firstlist.get(i),firstlist.get(j)))
+						possiblities.add(firstlist.get(i).size() + firstlist.get(j).size());
 			}
-
+			possiblities.add(maxi);
 			System.out.println(((TreeSet<Integer>) possiblities).last());
 
 		}
-	}
 
-	public static boolean compare(List<Integer> comp, List<Integer> first) {
-
-		for(int i=0;i<first.size();i++)
-			for(int j=0;j<comp.size();j++)
-				if(first.get(i) == comp.get(j))
-					return false;
-
-		return true;
-	}
+	}	
 }
